66ac80d9374e8f5c426b4e7d8f17c9cc39a67173
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 13:28:32 +0200

    wip

--- scripts/partials/user.sh
+++ scripts/partials/user.sh
@@ -3,21 +3,21 @@
 source /etc/jarvis/scripts/helpers.sh
 
 echo "Creating system user"
-useradd jarvis
-mkdir -p /home/jarvis/.ssh
-adduser jarvis sudo
-chsh -s /bin/bash jarvis
-cp /root/.profile /home/jarvis/.profile
-cp /root/.bashrc /home/jarvis/.bashrc
+execSilent useradd jarvis
+execSilent mkdir -p /home/jarvis/.ssh
+execSilent adduser jarvis sudo
+execSilent chsh -s /bin/bash jarvis
+execSilent cp /root/.profile /home/jarvis/.profile
+execSilent cp /root/.bashrc /home/jarvis/.bashrc
 
 echo "Copy source control public keys"
-ssh-keyscan -H github.com >> /home/jarvis/.ssh/known_hosts
-ssh-keyscan -H bitbucket.org >> /home/jarvis/.ssh/known_hosts
-ssh-keyscan -H gitlab.com >> /home/jarvis/.ssh/known_hosts
+execSilent ssh-keyscan -H github.com >> /home/jarvis/.ssh/known_hosts
+execSilent ssh-keyscan -H bitbucket.org >> /home/jarvis/.ssh/known_hosts
+execSilent ssh-keyscan -H gitlab.com >> /home/jarvis/.ssh/known_hosts
 
 echo "Setup directory permissions"
-chown -R jarvis:jarvis /home/jarvis
-chmod -R 755 /home/jarvis
+execSilent chown -R jarvis:jarvis /home/jarvis
+execSilent chmod -R 755 /home/jarvis
 
 echo "Allow php-fpm restart"
 echo "jarvis ALL=NOPASSWD: /usr/sbin/service php8.0-fpm reload" >> /etc/sudoers.d/php-fpm
