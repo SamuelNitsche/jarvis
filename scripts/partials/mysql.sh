5b0c67278ec88e13ebeaa641e0ce772edaa2ac19
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 19:16:49 +0200

    wip

--- scripts/partials/mysql.sh
+++ scripts/partials/mysql.sh
@@ -4,6 +4,6 @@ source /etc/jarvis/scripts/helpers.sh
 
 echo "Install mysql"
 export DEBIAN_FRONTEND=noninteractive
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 execSilent apt-get install mysql-server -y
