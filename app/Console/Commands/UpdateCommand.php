0be0af06aef5623114a78db4cb26f91c72b92540
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 17:31:43 +0200

    wip

---
+++ app/Console/Commands/UpdateCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Shell;
+use Illuminate\Console\Command;
+
+class UpdateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update jarvis';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        (new Shell)->execute('cd /etc/jarvis && git pull');
+        
+        return 0;
+    }
+}
