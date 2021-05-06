76ce5d74ef5ef2aecb42c395ddb2cecc3a6198e7
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 17:08:07 +0200

    wip

--- scripts/partials/nginx.sh
+++ scripts/partials/nginx.sh
@@ -12,7 +12,7 @@ sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
 sed -i "s/# server_tokens.*/server_tokens off;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
 sed -i "s/ssl_protocols.*/ssl_protocols TLSv1.2 TLSv1.3;/" /etc/nginx/nginx.conf
-execSilent curl https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/nginx/dhparams.pem
+curl https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/nginx/dhparams.pem
 execVerySilent rm /etc/nginx/sites-enabled/default
 execVerySilent rm /etc/nginx/sites-available/default
 execVerySilent rm -Rf /var/www/html/*
