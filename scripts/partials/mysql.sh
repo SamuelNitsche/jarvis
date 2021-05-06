3dffcd36d55055f974ae91eb5455317ef0b4a88b
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 19:06:20 +0200

    wip

--- scripts/partials/mysql.sh
+++ scripts/partials/mysql.sh
@@ -3,7 +3,6 @@
 source /etc/jarvis/scripts/helpers.sh
 
 echo "Install mysql"
-echo 'mysql-community-server mysql-community-server/root-pass password root' | debconf-set-selections
-echo 'mysql-community-server mysql-community-server/re-root-pass password root' | debconf-set-selections
-echo 'mysql-community-server mysql-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)' | debconf-set-selections
-execSilent apt-get install mysql-server -y
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+execSilent DEBIAN_FRONTEND=noninteractive apt-get install mysql-server -y
