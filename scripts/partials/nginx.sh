247522dd1560c5903858146a709ed3adc6c583ad
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 11:42:03 +0200

    wip

--- scripts/partials/nginx.sh
+++ scripts/partials/nginx.sh
@@ -10,7 +10,7 @@ sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
 sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
 sed -i "s/# server_tokens.*/server_tokens off;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
-sed -i "s/ssl_protocols.*/ssl_protocols TLSv1.2 TLSv1.3/" /etc/nginx/nginx.conf
+sed -i "s/ssl_protocols.*/ssl_protocols TLSv1.2 TLSv1.3;/" /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 rm -Rf /var/www/html/*
--- scripts/partials/ufw.sh
+++ scripts/partials/ufw.sh
@@ -6,7 +6,7 @@ echo "Installing ufw"
 execSilent apt install ufw -y
 
 echo "Setting initial firewall rules"
-execSilent ufw allow in \"Nginx Full\"
+execSilent ufw allow 'Nginx Full'
 execSilent ufw allow OpenSSH
 
 echo "Enable ufw"
