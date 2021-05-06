57a07ca0b11cad7e4b90902bd53c7054031eae4d
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 19:31:08 +0200

    wip

--- scripts/partials/mysql.sh
+++ scripts/partials/mysql.sh
@@ -9,6 +9,7 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 execSilent apt-get install mysql-server -y
 
 echo "Create jarvis user"
+echo
 mysql -uroot -p "$MYSQL_ROOT_PASSWORD" -e "CREATE USER 'jarvis'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysql -uroot -p "$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO 'jarvis'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 mysql -uroot -p "$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO 'jarvis'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
