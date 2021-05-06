ed5ee715a9fe7e822e5aa40a2aa357f6fbd5b31b
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 16:27:07 +0200

    wip

--- app/Services/Certbot.php
+++ app/Services/Certbot.php
@@ -7,11 +7,11 @@ class Certbot
     public static function requestCertificateForDomain($siteName, $dry)
     {
         $emailAddress = 'support@samys.network';
-        
+
         if ($dry) {
             (new Shell)->execute("certbot certonly --nginx -d {$siteName} --dry-run");
         } else {
-            (new Shell)->execute("certbot certonly --nginx -d {$siteName} -c {$emailAddress}");
+            (new Shell)->execute("certbot certonly --nginx -d {$siteName} -m {$emailAddress}");
         }
     }
 }
