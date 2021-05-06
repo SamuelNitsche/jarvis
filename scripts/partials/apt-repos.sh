79ed01f5686be4844ba445a99565ee240745bc47
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 11:35:03 +0200

    wip

--- scripts/partials/apt-repos.sh
+++ scripts/partials/apt-repos.sh
@@ -4,12 +4,9 @@ source /etc/stool/scripts/helpers.sh
 
 echo "Adding apt repository for nginx mainline"
 cat << EOF > /etc/apt/sources.list.d/nginx.list
-deb [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ focal nginx
-deb-src http://nginx.org/packages/mainline/ubuntu/ focal nginx
+deb http://ppa.launchpad.net/nginx/stable/ubuntu focal main
+deb-src http://ppa.launchpad.net/nginx/stable/ubuntu focal main
 EOF
-execSilent wget http://nginx.org/keys/nginx_signing.key
-execSilent apt-key add nginx_signing.key
-rm nginx_signing.key
 
 echo "Adding apt repository for php"
 execSilent add-apt-repository ppa:ondrej/php -y
--- scripts/partials/nginx.sh
+++ scripts/partials/nginx.sh
@@ -6,10 +6,11 @@ echo "Install nginx"
 execSilent apt install nginx -y
 
 echo "Initial nginx configuration"
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
 sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
+sed -i "s/# server_tokens.*/server_tokens off;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
+sed -i "s/ssl_protocols.*/ssl_protocols TLSv1.2 TLSv1.3/" /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 rm -Rf /var/www/html/*
