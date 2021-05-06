8effad0ad5c2b2eb05189190b65dab78322f1122
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 11:01:16 +0200

    wip

--- scripts/partials/nginx.sh
+++ scripts/partials/nginx.sh
@@ -6,8 +6,10 @@ echo "Install nginx"
 execSilent apt install nginx -y
 
 echo "Initial nginx configuration"
-rm /etc/nginx/nginx.conf
-cp /etc/stool/templates/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf
+sed -i "s/# multi_accept.*/multi_accept on;/" /etc/nginx/nginx.conf
+sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 128;/" /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 rm -Rf /var/www/html/*
