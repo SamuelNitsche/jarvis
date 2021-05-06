699821bed7ccf0bd8b7e0d5a7e27b17be0e252c2
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 11:54:22 +0200

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,15 @@
 
 source /etc/stool/scripts/helpers.sh
 
+cat << "EOF"
+         __              __
+   _____/ /_____  ____  / /
+  / ___/ __/ __ \/ __ \/ /
+ (__  ) /_/ /_/ / /_/ / /
+/____/\__/\____/\____/_/
+    By Samuel Nitsche
+EOF
+
 echo "Initialization..."
 bash /etc/stool/scripts/partials/init.sh
 
