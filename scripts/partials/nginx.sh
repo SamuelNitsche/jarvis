70009c4ea5a4507b70b544151417c1d4a6847d5b
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 13:38:48 +0200

    wip

--- scripts/partials/nginx.sh
+++ scripts/partials/nginx.sh
@@ -15,3 +15,23 @@ sed -i "s/ssl_protocols.*/ssl_protocols TLSv1.2 TLSv1.3;/" /etc/nginx/nginx.conf
 execVerySilent rm /etc/nginx/sites-enabled/default
 execVerySilent rm /etc/nginx/sites-available/default
 execVerySilent rm -Rf /var/www/html/*
+execVerySilent mkdir -p /home/jarvis/localhost/public/
+execSilent cp /etc/jarvis/templates/nginx/index.php /home/jarvis/localhost/public/index.php
+cat << EOF > /etc/nginx/sites-available/localhost
+server {
+  listen 80 default_server;
+  root /home/jarvis/localhost/public;
+  index index.php;
+
+  location ~ \.php$ {
+    try_files $uri /index.php =404;
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    fastcgi_pass unix:/run/php/php8.0-fpm.sock;
+    fastcgi_buffers 16 16k;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include fastcgi_params;
+  }
+}
+execSilent ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
+EOF
---
+++ templates/nginx/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>It work's</title>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div
+      class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+      <div class="max-w-md mx-auto">
+        It works!
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
