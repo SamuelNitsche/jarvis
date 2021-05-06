f100eee0ff2a27d5077b64bbe4865489577814bb
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 13:58:49 +0200

    wip

--- scripts/partials/composer.sh
+++ scripts/partials/composer.sh
@@ -3,5 +3,5 @@
 source /etc/jarvis/scripts/helpers.sh
 
 echo "Installing composer"
-execSilent curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php
 execSilent mv composer.phar /usr/bin/composer
