51e3a319d73c1d2efb73b2e9c127b78df121aa35
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 18:37:02 +0200

    wip

--- scripts/partials/mysql.sh
+++ scripts/partials/mysql.sh
@@ -3,12 +3,14 @@
 source /etc/jarvis/scripts/helpers.sh
 
 echo "Install mysql"
-debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-apt-get -y install mysql-server
 
-mysql -u root -proot -e "UPDATE mysql.user SET authentication_string=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'"
-mysql -u root -proot -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -proot -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -proot -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-mysql -u root -proot -e "FLUSH PRIVILEGES"
+debconf-set-selections <<< \
+  "mysql-community-server mysql-community-server/root-pass password $MYSQL_ROOT_PASSWORD"
+
+debconf-set-selections <<< \
+  "mysql-community-server mysql-community-server/re-root-pass password $MYSQL_ROOT_PASSWORD"
+
+debconf-set-selections <<< \
+  "mysql-community-server mysql-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)"
+
+DEBIAN_FRONTEND=noninteractive apt-get install mysql-server -y
