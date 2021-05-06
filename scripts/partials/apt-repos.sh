cdb4c857b6915e3397f093e7ee33e322244253eb
Samuel Nitsche <mail@samynitsche.de>
2021-05-06 10:11:40 +0200

    wip

--- scripts/partials/apt-repos.sh
+++ scripts/partials/apt-repos.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "Adding apt repository for nginx mainline"
-add-apt-repository ppa:nginx/development
+cat << EOF > /etc/apt/sources.list.d/nginx.list
+deb [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ focal nginx
+deb-src http://nginx.org/packages/mainline/ubuntu/ focal nginx
+EOF
+wget http://nginx.org/keys/nginx_signing.key
+apt-key add nginx_signing.key
+rm nginx_signing.key
 
 echo "Adding apt repository for php"
 add-apt-repository ppa:ondrej/php
+
+apt update -y
